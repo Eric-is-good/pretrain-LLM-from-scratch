87ac464e7b7be3934bf07a49ae538c9ad39a164e
Justin Li <justinlii@outlook.com>
2024-11-03 22:49:29 +0800

    qwq

--- data/prepare_skypile.py
+++ data/prepare_skypile.py
@@ -15,35 +15,15 @@ class SkyDataProcess(DataProcess):
         # 获取所有json文件
         self.data_files = [os.path.join(data_dir, file) for file in os.listdir(data_dir) if file.endswith(".jsonl")]
     
-    @staticmethod
-    def __amend_to_ndarray(sub_array: np.ndarray, array: np.ndarray | None = None) -> np.ndarray:
-        if array is None:
-            return sub_array.reshape(1, -1).astype(np.uint16)
-        else:    
-            return np.concatenate((array, sub_array.reshape(1, -1).astype(np.uint16)), axis=0)
-    
-    # @staticmethod    
-    # def __count_lines(filename):
-    #     """在不完全加载文件的情况下计算文件行数"""
-    #     import time
-    #     import mmap
-    #     filename = "E:\\Projects\\HolmesLM\\dataset\\skypile\\2021-17_zh_middle_0002.jsonl"
-    #     start_time = time.time()
-    #     with open(filename, 'r') as f:
-    #         buf = mmap.mmap(f.fileno(), 0, access=mmap.ACCESS_READ)
-    #         ret = buf.count(b'\n')
-    #         end_time = time.time()
-    #         print(f"Time taken to count lines in {filename}: {end_time - start_time} seconds")
-    #         return ret
-    
     def precess_one_file(self, data_path):
         # 每一行都是一个json对象，读取里面的text字段
         npy_file_name = data_path.replace(".jsonl", "")
-        array = None
+        array = []
         with open(data_path, "r", encoding="utf-8") as f:
             current_tokens = []
             # for every sentence
-            for line in tqdm(f,total=410000, desc=f"Processing {data_path}, 总数仅供参考"):
+            # 多线程tqdm会有显示问题，所以这里不用tqdm
+            for line in tqdm(f):
                 sentence = json.loads(line)["text"]
                 tokens = self.tokenize_sentense(sentence)
                 # append to current_tokens
@@ -52,7 +32,7 @@ class SkyDataProcess(DataProcess):
                     current_tokens += [self.tokenizer.pad_token_id] * (self.max_length - len(current_tokens))
                     # 写入numpy数组
                     npy = self.convert_list_to_numpy(current_tokens)
-                    array = self.__amend_to_ndarray(npy, array)
+                    array.append(npy)
                     # 开始新行
                     current_tokens = tokens
                 else:
@@ -62,9 +42,9 @@ class SkyDataProcess(DataProcess):
         if current_tokens:
             current_tokens += [self.tokenizer.pad_token_id] * (self.max_length - len(current_tokens))
             npy = self.convert_list_to_numpy(current_tokens)
-            array = self.__amend_to_ndarray(npy, array)
+            array.append(npy)
         
-        assert array is not None
+        array = np.stack(array, axis=0)
         np.save(npy_file_name, array)
             
         print(f"Save {npy_file_name} successfully. Total {array.shape[0]} sentences.")
