57c7b86a6b9a88283db8dfcb8c6c7f804ce5677c
JustinLii <JustinLii@outlook.com>
2024-11-07 16:51:01 +0800

    depadding p2

--- data/skypile/depadding.py
+++ data/skypile/depadding.py
@@ -1,6 +1,7 @@
 """Transform padded data seq to unpadded continuous seq
 """
 import numpy as np
+from transformers import LlamaTokenizer
 
 def append_line(tokens: np.ndarray, current_tokens: list, seq_length) -> tuple[list, list|None]:
     tokens: list = list(tokens)
@@ -35,5 +36,16 @@ def depadding(data: np.ndarray, pad_token_id, seq_length=4096, context=200):
             else:
                 current_tokens = []
                 
-    if current_tokens:
-        new_data
+    # 抛弃最后一个不足长度的行
+    if len(current_tokens) == seq_length:
+        new_data.append(np.array(current_tokens, dtype=np.uint16))
+        
+    return np.stack(new_data, axis=0)
+
+def main():
+    tokenizer = LlamaTokenizer.from_pretrained("model/", use_fast=True)
+    pad_token_id = tokenizer.pad_token_id
+    
+    data = np.load("data.npy") # TODO: find the data file
+    new_data = depadding(data, pad_token_id)
+    np.save("new_data.npy", new_data) # TODO: save at the right place
