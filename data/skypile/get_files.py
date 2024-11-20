f42ee11492d00c4177556277173f2dc7b416fefb
Justin Li <justinlii@outlook.com>
2024-11-20 21:01:34 +0800

    new get_files

--- data/skypile/get_files.py
+++ data/skypile/get_files.py
@@ -1,5 +1,5 @@
 import os
-from concurrent.futures import ProcessPoolExecutor
+from concurrent.futures import ThreadPoolExecutor
 
 import wget
 
@@ -12,13 +12,17 @@ def get_file(url):
 
 if __name__ == "__main__":
     
-    with open("url2.txt", "r", encoding="utf-8") as f:
+    with open("data\\skypile\\url2.txt", "r", encoding="utf-8") as f:
         urls = f.readlines()
         
     existed_files = [file for file in os.listdir(FILE_DIR) if file.endswith(".jsonl")]
 
     urls = [url.strip() for url in urls if url.strip().split("/")[-1] not in existed_files]
     total = len(urls)
-    with ProcessPoolExecutor(max_workers=10) as executor:
+    with ThreadPoolExecutor() as executor:
         for i, _ in enumerate(executor.map(get_file, urls)):
-            print(f"\n{i}/{total}")
+            print(f"\n{i+1}/{total}")
+    
+    # with open("data\\skypile\\url2.txt", "a", encoding="utf-8") as f:
+    #     for i in range(7):
+    #         f.write(f"https://huggingface.co/datasets/Skywork/SkyPile-150B/resolve/main/data/2020-50_zh_head_00{i:02d}.jsonl\n")
