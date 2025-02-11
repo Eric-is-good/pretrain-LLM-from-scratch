35881e0b4376672459680fef1cf43e7d5ba7556d
ZHU PEIYIN <eric123456123456@163.com>
2025-02-11 10:22:07 +0800

    Update grpo_train.py

--- train/grpo_train.py
+++ train/grpo_train.py
@@ -5,6 +5,7 @@ import pandas as pd
 from datasets import Dataset
 import json
 import re
+import os
 
 ##########################################################################################################
 # process data
@@ -47,14 +48,23 @@ print(dataset[0])
 # # reward model/fuctions ( only fuctions @_@ )
 
 def length_reward_func(completions, **kwargs):
-    reward = [float(len(completion[0]["content"]))*0.002 for completion in completions]
-    return reward
+    score = [float(len(completion[0]["content"]))*0.002 for completion in completions]
+    
+    # print("##############################")
+    # print(score)
+    
+    return score
 
 def format_reward_func(completions, **kwargs):
     pattern = r"^<think>.*?</think>\s*<answer>.*?</answer>$"
     completion_contents = [completion[0]["content"] for completion in completions]
-    matches = [re.match(pattern, content, re.DOTALL)for content in completion_contents]
-    return [1.0 if match else 0.0 for match in matches]
+    matches = [re.match(pattern, content, re.DOTALL) for content in completion_contents]
+    score = [1.5 if match else -0.5 for match in matches]
+    
+    # print("##############################")
+    # print(score)
+    
+    return score
 
 def answer_reward_func(completions, answer, **kwargs):
     completions_matches = [re.search(r"####\s*(\d+)", str(c)) for c in completions]
@@ -62,21 +72,47 @@ def answer_reward_func(completions, answer, **kwargs):
     
     ground_truth_matches = [re.search(r"####\s*(\d+)", str(c)) for c in answer]
     ground_truth_ = [match.group(1) if match else "" for match in ground_truth_matches]
-    
-    return [2.0 if c.strip() == gt.strip() else 0.0 for c, gt in zip(completions_, ground_truth_)]
 
+    score = [3.0 if c.strip() == gt.strip() else -1.0 for c, gt in zip(completions_, ground_truth_)]
+    
+    # print("##############################")
+    # print(score)
+    
+    # 选择一个得了3分且 completions 最长的打印
+    # Find completions with a score of 3
+    best_answer = None
+    max_length = 0
+    
+    for c, s in zip(completions, score):
+        if s == 3.0:  # If the score is 3
+            if len(c) > max_length:  # Check if it's the longest one
+                max_length = len(c)
+                best_answer = c
+
+    # Print the best match (longest one that scored 3)
+    if best_answer:
+        print(best_answer)
+    
+    
+    return score
 
 ##########################################################################################################
 # training args
+os.environ["WANDB_PROJECT"] = "TRL"
 
 training_args = GRPOConfig(
-    output_dir="Holmes-0.5B-GRPO", 
-    run_name="Holmes-0.5B-GRPO",
+    output_dir="GRPO", 
+    run_name="GRPO",
+    per_device_train_batch_size=1,
+    gradient_accumulation_steps=2,
+    use_vllm=True,
+    max_prompt_length=1024,
+    max_completion_length=2048,
     bf16=True,
-    save_steps=100,
-    max_grad_norm=0.1,
-    report_to="none",
-    logging_steps=10
+    save_steps=1000,
+    max_grad_norm=1.0,
+    report_to="wandb",
+    logging_steps=1
 )
 
 trainer = GRPOTrainer(
